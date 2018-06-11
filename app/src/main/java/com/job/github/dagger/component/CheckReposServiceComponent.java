bdf89f33fcc5e4ab87a50aba1e86e0cae4dbf88b
Vladislav <slavayus@gmail.com>
2018-06-11 18:05:34 +0300

    inject NotificationHelper to the service

---
+++ app/src/main/java/com/job/github/dagger/component/CheckReposServiceComponent.java
@@ -0,0 +1,18 @@
+package com.job.github.dagger.component;
+
+import com.job.github.dagger.module.CheckReposServiceModule;
+import com.job.github.utils.CheckReposJobService;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * Created by slavik on 6/11/18.
+ */
+
+@Singleton
+@Component(modules = {CheckReposServiceModule.class})
+public interface CheckReposServiceComponent {
+    void injectReposService(CheckReposJobService service);
+}
--- app/src/main/java/com/job/github/utils/CheckReposJobService.java
+++ app/src/main/java/com/job/github/utils/CheckReposJobService.java
@@ -3,11 +3,14 @@ package com.job.github.utils;
 import android.app.PendingIntent;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
-import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import com.job.github.dagger.component.DaggerCheckReposServiceComponent;
+import com.job.github.dagger.module.ApplicationContextModule;
 import com.job.github.mvp.view.LaunchActivity;
+
+import javax.inject.Inject;
 
 /**
  * Created by slavik on 6/11/18.
@@ -15,18 +18,20 @@ import com.job.github.mvp.view.LaunchActivity;
 
 public class CheckReposJobService extends JobService {
     private static final String TAG = "CheckReposJobService";
-    private final NotificationHelper notification;
-
-    public CheckReposJobService() {
-        this.notification = new NotificationHelper(this);
-    }
+    @Inject NotificationHelper notification;
 
     @Override
     public boolean onStartJob(JobParameters params) {
         Log.d(TAG, "onStartJob: ");
-        Context context = getApplicationContext();
-        Intent intent = LaunchActivity.newInstance(context);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        DaggerCheckReposServiceComponent
+                .builder()
+                .applicationContextModule(new ApplicationContextModule(this))
+                .build()
+                .injectReposService(this);
+
+        Intent intent = LaunchActivity.newInstance(this);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
         new Thread(() -> {
             try {
                 Thread.sleep(1000);
